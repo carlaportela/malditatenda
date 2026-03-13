a109519c3edd01fa1377ae5eee925c65db0d243e
Carla Portela Ubeira 🔰 <120684708+carlaportela@users.noreply.github.com>
2026-03-13 07:49:49 +0100

    Se elimina la animación de la cesta cuando se está en la página de la cesta

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -80,15 +80,25 @@
             
             <!-- Cesta -->
             <div class="flex items-center space-x-4">
-                <div class="relative transition-transform duration-200 hover:scale-110 hover:opacity-80 transition-opacity duration-200">
+                <div class="relative transition-transform duration-200 transition-opacity duration-200
+                    {{ request()->is('canastro') 
+                        ? 'opacity-60 pointer-events-none cursor-default' 
+                        : 'hover:scale-110 hover:opacity-80' }}">
+
                     <a href="/canastro"
-                    class="inline-block transition-transform duration-200 hover:scale-110
-                    {{ request()->is('canastro') ? 'opacity-60 pointer-events-none cursor-default' : 'hover:opacity-80' }}">
-                        <img src="{{ asset('assets/img/logos/icono_cesta.png')}}" alt="Cesta" class="w-6 h-6">
+                    class="inline-block
+                    {{ request()->is('canastro') ? 'pointer-events-none cursor-default' : '' }}">
+                    
+                        <img src="{{ asset('assets/img/logos/icono_cesta.png')}}" 
+                        alt="Cesta" 
+                        class="w-6 h-6">
                     </a>
-                    <span id="contadorCesta" class="absolute -top-2 -right-2 bg-red-400 text-white text-xs px-1.5 rounded-full pointer-events-none">
+
+                    <span id="contadorCesta"
+                    class="absolute -top-2 -right-2 bg-red-400 text-white text-xs px-1.5 rounded-full pointer-events-none">
                         {{ $contadorCesta }}
                     </span>
+
                 </div>
             </div>
         </div>
